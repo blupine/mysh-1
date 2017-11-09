2ea1d6fbc385924546546477a47fb38215341ce0
blupine <skwint11@naver.com>
2017-11-09 20:34:50 +0900

    ipc all implemented, need to modify signal

--- include/commands.h
+++ include/commands.h
@@ -10,5 +10,6 @@ struct single_command
 int evaluate_command(int n_commands, struct single_command (*commands)[512]);
 
 void free_commands(int n_commands, struct single_command (*commands)[512]);
-
+int exec_com(struct single_command (*com));
+void* socket_thread(void* argv);
 #endif // MYSH_COMMANDS_H_
